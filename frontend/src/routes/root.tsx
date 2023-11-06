a4ea0678f73ebc46b84cd0dd7d09901f041edb4a
szabot1 <gleams-aquifer.0d@icloud.com>
2023-11-06 11:42:37 +0100

    feat(frontend): dotenv loader

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -5,6 +5,9 @@ export default function RootPage() {
     <div className="flex flex-col">
       <Link to="/products">Products</Link>
       <Link to="/products/1">Product 1</Link>
+
+      <p>{import.meta.env.VITE_FRONTEND_PROD_URL}</p>
+      <p>{import.meta.env.VITE_BACKEND_PROD_URL}</p>
     </div>
   );
 }
