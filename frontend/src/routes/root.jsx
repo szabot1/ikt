562a9185e4d8011c09f8c006802bf3ebc10a2de3
szabot1 <gleams-aquifer.0d@icloud.com>
2023-11-06 09:38:19 +0100

    initial commit

---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,8 @@
+export default function RootPage() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Hello daisyUI</button>
+    </>
+  );
+}
