59c58331e77431fe7d16a66723ae2a2629e2cf47
szabot1 <gleams-aquifer.0d@icloud.com>
2023-11-21 08:25:00 +0100

    fix: migration

--- db/migrations/01_reset.sql
+++ db/migrations/01_reset.sql
@@ -1,6 +1,7 @@
 drop table if exists games cascade;
 drop table if exists game_images cascade;
 drop table if exists game_tags cascade;
+drop table if exists offer_types cascade;
 drop table if exists offers cascade;
 drop type if exists order_status cascade;
 drop table if exists orders cascade;
