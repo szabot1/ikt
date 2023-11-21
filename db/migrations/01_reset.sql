e03d3e63d91979dacc7790bf4e6c46caeec76cef
szabot1 <gleams-aquifer.0d@icloud.com>
2023-11-21 08:21:08 +0100

    chore: drop order status

--- db/migrations/01_reset.sql
+++ db/migrations/01_reset.sql
@@ -4,6 +4,7 @@ drop table if exists games;
 drop table if exists game_images;
 drop table if exists game_tags;
 drop table if exists offers;
+drop type if exists order_status;
 drop table if exists orders;
 drop table if exists sellers;
 drop table if exists tags;
