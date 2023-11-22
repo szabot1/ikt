72ad689b9a1fc11b1a999ac5c3ac5bf7a94ea742
szabot1 <gleams-aquifer.0d@icloud.com>
2023-11-22 10:35:28 +0100

    chore: test link

--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -5,6 +5,7 @@ export default function RootPage() {
     <div className="flex flex-col">
       <Link to="/weather">Weather</Link>
       <Link to="/products/1">Product 1</Link>
+      <Link to="/test">Test</Link>
 
       <p>{import.meta.env.VITE_FRONTEND_PROD_URL}</p>
       <p>{import.meta.env.VITE_BACKEND_PROD_URL}</p>
