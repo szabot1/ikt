4f88650bb5704c2d0b478d1961122b8c176ef968
szabot1 <gleams-aquifer.0d@icloud.com>
2024-02-02 11:50:05 +0100

    feat: add basic auth design

--- frontend/src/routes/auth.register.tsx
+++ frontend/src/routes/auth.register.tsx
@@ -3,21 +3,29 @@ import { useAuth } from "@/lib/auth";
 import { FileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-const signInSchema = z.object({
+const registerSchema = z.object({
   redirect: z.string().optional(),
 });
 
 export const Route = new FileRoute("/auth/register").createRoute({
   component: Register,
   errorComponent: ErrorPage,
-  validateSearch: (search) => signInSchema.parse(search),
+  validateSearch: (search) => registerSchema.parse(search),
   beforeLoad: async ({ context: { auth } }) => {
     if (auth.isAuthenticated) {
       throw redirect({ to: "/" });
     }
   },
 });
+
+type RegisterInputs = {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
 
 function Register() {
   const auth = useAuth();
@@ -27,14 +35,57 @@ function Register() {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<RegisterInputs>();
+
+  const onSubmit: SubmitHandler<RegisterInputs> = (data) => console.log(data);
 
   return (
     <section className="grow flex items-center justify-center">
       <div className="px-6 py-3 border-2 border-gray-700 rounded-lg">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email</label>
+            <input
+              className="px-2 py-1 bg-transparent rounded-md border-2 border-gray-700 transition-all duration-200 focus:border-green-600 ring-0 focus:ring-0 outline-none focus:outline-none"
+              type="email"
+              {...register("email", { required: true })}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="username">Username</label>
+            <input
+              className="px-2 py-1 bg-transparent rounded-md border-2 border-gray-700 transition-all duration-200 focus:border-green-600 ring-0 focus:ring-0 outline-none focus:outline-none"
+              {...register("username", { required: true })}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="px-2 py-1 bg-transparent rounded-md border-2 border-gray-700 transition-all duration-200 focus:border-green-600 ring-0 focus:ring-0 outline-none focus:outline-none"
+              {...register("password", { required: true })}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              className="px-2 py-1 bg-transparent rounded-md border-2 border-gray-700 transition-all duration-200 focus:border-green-600 ring-0 focus:ring-0 outline-none focus:outline-none"
+              {...register("confirmPassword", {
+                required: true,
+                validate: (value) => value === watch("password"),
+              })}
+            />
+          </div>
+
           <button
             type="submit"
             className="px-6 py-3 bg-green-700 rounded-lg hover:bg-green-600 transition-all duration-200"
--- frontend/src/routes/auth.signin.tsx
+++ frontend/src/routes/auth.signin.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "@/lib/auth";
 import { FileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const signInSchema = z.object({
   redirect: z.string().optional(),
@@ -19,6 +20,11 @@ export const Route = new FileRoute("/auth/signin").createRoute({
   },
 });
 
+type LoginInputs = {
+  email: string;
+  password: string;
+};
+
 function SignIn() {
   const auth = useAuth();
   const navigate = useNavigate();
@@ -27,14 +33,37 @@ function SignIn() {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<LoginInputs>();
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => console.log(data);
 
   return (
     <section className="grow flex items-center justify-center">
       <div className="px-6 py-3 border-2 border-gray-700 rounded-lg">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email</label>
+            <input
+              className="px-2 py-1 bg-transparent rounded-md border-2 border-gray-700 transition-all duration-200 focus:border-green-600 ring-0 focus:ring-0 outline-none focus:outline-none"
+              type="email"
+              {...register("email", { required: true })}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="px-2 py-1 bg-transparent rounded-md border-2 border-gray-700 transition-all duration-200 focus:border-green-600 ring-0 focus:ring-0 outline-none focus:outline-none"
+              {...register("password", { required: true })}
+            />
+          </div>
+
           <button
             type="submit"
             className="px-6 py-3 bg-green-700 rounded-lg hover:bg-green-600 transition-all duration-200"
