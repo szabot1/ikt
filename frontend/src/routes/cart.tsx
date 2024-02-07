e54528386dff9fce65da9aa5637d6571d2e279c9
szabot1 <gleams-aquifer.0d@icloud.com>
2024-02-07 09:50:05 +0100

    feat: add cart and settings

--- frontend/src/routes/cart.tsx
+++ frontend/src/routes/cart.tsx
@@ -7,5 +7,50 @@ export const Route = new FileRoute("/cart").createRoute({
 });
 
 function Cart() {
-  return <></>;
+  return (
+    <section className="grow flex flex-col lg:flex-row gap-4 lg:gap-12 items-center justify-center">
+      <div className="flex flex-col gap-2 w-10/12 md:w-6/12 lg:w-3/12">
+        <h1 className="text-2xl font-semibold">Cart</h1>
+
+        <div className="px-12 py-6 border-2 border-zinc-700 rounded-lg">
+          <span className="text-red-500 text-sm">
+            You have no items in your cart
+          </span>
+        </div>
+      </div>
+
+      <div className="px-12 py-6 border-2 border-green-700 rounded-lg w-10/12 md:w-6/12 lg:w-3/12 flex flex-col gap-2">
+        <button
+          type="submit"
+          disabled
+          className="py-2 bg-green-700 rounded-lg hover:bg-green-600 transition-all duration-200 flex items-center justify-center disabled:opacity-50 disabled:hover:bg-green-700 disabled:cursor-not-allowed"
+        >
+          Checkout
+        </button>
+
+        <span className="text-zinc-400 text-sm text-justify">
+          Payments are handled and secured by Stripe. By clicking "Checkout",
+          you agree to our{" "}
+          <a
+            href="/terms"
+            target="_blank"
+            rel="noreferrer"
+            className="text-green-500 hover:underline transition-all duration-200 hover:text-green-400"
+          >
+            terms and conditions
+          </a>
+          . We do not store any of your payment information, visit{" "}
+          <a
+            href="https://stripe.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-green-500 hover:underline transition-all duration-200 hover:text-green-400"
+          >
+            stripe.com
+          </a>{" "}
+          for more information.
+        </span>
+      </div>
+    </section>
+  );
 }
--- frontend/src/routes/settings.tsx
+++ frontend/src/routes/settings.tsx
@@ -7,5 +7,51 @@ export const Route = new FileRoute("/settings").createRoute({
 });
 
 function Settings() {
-  return <></>;
+  return (
+    <section className="grow flex flex-col lg:flex-row gap-4 lg:gap-12 items-center justify-center">
+      <div className="px-6 py-3 border-2 border-zinc-700 rounded-lg w-10/12 md:w-6/12 lg:w-3/12 flex flex-col gap-2">
+        <h1 className="text-xl font-semibold mb-4">Account</h1>
+
+        <div className="w-full flex flex-col gap-2">
+          <button className="w-full py-2 bg-red-700 rounded-lg hover:bg-red-600 transition-all duration-200 flex items-center justify-center">
+            Delete account
+          </button>
+
+          <span className="text-zinc-400 text-sm text-justify">
+            Click the button to delete your account. This action is irreversible
+            and will delete all your data. If you would like to export your data
+            first, please{" "}
+            <a
+              href="/tickets"
+              target="_blank"
+              rel="noreferrer"
+              className="text-green-500 hover:underline transition-all duration-200 hover:text-green-400"
+            >
+              contact support
+            </a>
+            .
+          </span>
+        </div>
+      </div>
+
+      <div className="px-6 py-3 border-2 border-green-700 rounded-lg w-10/12 md:w-6/12 lg:w-3/12 flex flex-col gap-2">
+        <h1 className="text-xl font-semibold mb-4">Billing</h1>
+
+        <a
+          href="https://stripe.com"
+          target="_blank"
+          rel="noreferrer"
+          className="py-2 bg-green-700 rounded-lg hover:bg-green-600 transition-all duration-200 flex items-center justify-center"
+        >
+          Manage billing
+        </a>
+
+        <span className="text-zinc-400 text-sm text-justify">
+          Billing information is handled and secured by Stripe. Click the button
+          to go to the Stripe Customer Portal where you can manage your billing
+          information.
+        </span>
+      </div>
+    </section>
+  );
 }
