72f4de18d86ecbfbe2854398b19aee269c8a2c1d
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2024-02-08 09:45:23 +0100

    --add game.path

--- frontend/src/routes/game.$path.tsx
+++ frontend/src/routes/game.$path.tsx
@@ -3,6 +3,7 @@ import { Game, gameQuery } from "@/lib/query/games";
 import { seoPathKey } from "@/lib/seo-path";
 import { useQuery } from "@tanstack/react-query";
 import { FileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
 export const Route = new FileRoute("/game/$path").createRoute({
   component: GameComponent,
@@ -16,17 +17,43 @@ function GameComponent() {
   const { data, isLoading } = useQuery(gameQuery(gameId));
   const game = data as Game | undefined;
 
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count + 1 == game?.images.length) {
+        setCount(0);
+      } else {
+        setCount(count + 1);
+      }
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [count]);
+
   return (
     <div>
-      <h1>Game</h1>
-      <p>Game ID: {gameId}</p>
-
       {isLoading && <p>Loading...</p>}
 
       {!isLoading && game && (
-        <code className="bg-orange-400 whitespace-pre-wrap">
-          {JSON.stringify({ game, isLoading }, null, 2)}
-        </code>
+        <section className="grow flex items-center justify-center">
+          <div className="max-w-sm border-2 rounded-2xl border-zinc-700 overflow-hidden shadow-lg">
+            <img
+              className="w-full rounded-t-2xl"
+              src={game.images[count].imageUrl}
+              alt={game.name}
+            ></img>
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{game.name}</div>
+              <p className="text-zinc-300 text-base">{game.description}</p>
+              <br></br>
+              {game.tags.map((tag) => (
+                <span className="inline-block bg-inherit rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                  #{tag.tag.name}
+                </span>
+              ))}
+            </div>
+          </div>
+        </section>
       )}
     </div>
   );
