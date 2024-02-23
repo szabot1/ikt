e880d9f3240461ee1bc2b3c838e1c5bd8b16943a
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2024-02-23 09:22:25 +0100

    -add gamepath buyer stage

--- frontend/src/routes/game.$path.tsx
+++ frontend/src/routes/game.$path.tsx
@@ -51,25 +51,43 @@ function GameComponent() {
       {isLoading && <p>Loading...</p>}
 
       {!isLoading && game && (
-        <section className="grow flex items-center justify-center mt-8">
-          <div className="max-w-sm border-2 rounded-2xl border-zinc-700 overflow-hidden shadow-lg">
-            <img
-              className="w-full rounded-t-2xl"
-              src={game.images[count].imageUrl}
-              alt={game.name}
-            ></img>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{game.name}</div>
-              <p className="text-zinc-300 text-base">{game.description}</p>
-              <br></br>
-              {game.tags.map((tag) => (
-                <span className="inline-block bg-inherit rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-                  #{tag.tag.name}
-                </span>
-              ))}
+        <div className="flex items-center justify-center gap-4">
+          <section className="flex items-start justify-start mt-8 ml-5">
+            <div className="max-w-sm border-2 rounded-2xl border-zinc-700 overflow-hidden shadow-lg">
+              <img
+                className="w-full rounded-t-2xl"
+                src={game.images[count].imageUrl}
+                alt={game.name}
+              ></img>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{game.name}</div>
+                <p className="text-zinc-300 text-base">{game.description}</p>
+                <br></br>
+                {game.tags.map((tag) => (
+                  <span
+                    key={tag.tag.name}
+                    className="inline-block bg-inherit rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2"
+                  >
+                    #{tag.tag.name}
+                  </span>
+                ))}
+              </div>
             </div>
+          </section>
+          <div className="grid grid-cols-1 place-items-right divide-y ml-5 mt-8 divide-slate-700">
+            {game.tags.map((tag) => (
+              <div className="py-6 flex gap-2">
+                <span>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                </span>
+                <button className="bg-green-600 text-gray font-bold py-2 px-5  hover:border-gray-600 rounded-lg  mr-2">
+                  Buy
+                </button>
+              </div>
+            ))}
           </div>
-        </section>
+        </div>
       )}
     </div>
   );
