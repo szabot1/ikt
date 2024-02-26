9fe371719246ccd7f701e8173c35d4d5ed3a4593
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2024-02-26 09:08:31 +0100

    Add offer.ts

---
+++ frontend/src/lib/query/offer.ts
@@ -0,0 +1,10 @@
+export type Offer = {
+    id: string;
+    gameId: string;
+    sellerId: string;
+    price: number;
+    isActive: boolean;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+}
