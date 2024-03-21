0e8c81aecbc8be3c18213d50b346780bbd4d9b58
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2024-03-21 09:11:37 +0100

    checkout successpage

--- frontend/src/routes/checkout.cancel.tsx
+++ frontend/src/routes/checkout.cancel.tsx
@@ -1,5 +1,8 @@
 import ErrorPage from "@/error-page";
+import { userInfoQuery } from "@/lib/query/auth";
+import { useQuery } from "@tanstack/react-query";
 import { FileRoute, redirect } from "@tanstack/react-router";
+import { type User as UserInfo } from "@/lib/query/auth";
 
 export const Route = new FileRoute("/checkout/cancel").createRoute({
   component: CheckoutSuccess,
@@ -17,5 +20,30 @@ export const Route = new FileRoute("/checkout/cancel").createRoute({
 });
 
 function CheckoutSuccess() {
-  return <span>Checkout cancelled</span>;
+  const { data, isLoading } = useQuery(userInfoQuery());
+  const userInfo = data as UserInfo;
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="px-6 py-3 border-2 border-red-700 rounded-lg w-10/12 md:w-6/12 lg:w-3/12 flex flex-col gap-2">
+        <h1 className="text-xl font-semibold mb-4">Unsuccessful Payment</h1>
+        <p className="mb-5">Dear {userInfo.username},</p>
+        <p className="mb-5">
+          We regret to inform you that your recent payment attempt was
+          unsuccessful. We apologize for any inconvenience this may have caused.
+          There could be various reasons for the failed transaction, including
+          insufficient funds, incorrect payment details, or issues with the
+          payment processor. We kindly ask you to double-check the payment
+          information provided and ensure that all details are accurate. If the
+          issue persists, we recommend contacting your bank or payment provider
+          for further assistance. If you have any questions or require
+          assistance, please don't hesitate to reach out to our customer support
+          team. We're here to help resolve any issues and ensure a smooth
+          shopping experience for you. Thank you for your understanding.
+        </p>
+        <p>Best regards, </p>
+        <p className="mb-5">Game Key Store Team</p>
+      </div>
+    </div>
+  );
 }
--- frontend/src/routes/checkout.success.tsx
+++ frontend/src/routes/checkout.success.tsx
@@ -1,5 +1,7 @@
 import ErrorPage from "@/error-page";
 import { FileRoute, redirect } from "@tanstack/react-router";
+import { userInfoQuery, type User as UserInfo } from "@/lib/query/auth";
+import { useQuery } from "@tanstack/react-query";
 
 export const Route = new FileRoute("/checkout/success").createRoute({
   component: CheckoutSuccess,
@@ -17,5 +19,27 @@ export const Route = new FileRoute("/checkout/success").createRoute({
 });
 
 function CheckoutSuccess() {
-  return <span>Checkout success</span>;
+  const { data, isLoading } = useQuery(userInfoQuery());
+  const userInfo = data as UserInfo;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="px-6 py-3 border-2 border-green-700 rounded-lg w-10/12 md:w-6/12 lg:w-3/12 flex flex-col gap-2">
+        <h1 className="text-xl font-semibold mb-4">Thank you</h1>
+        <p className="mb-1">
+          <p className="mb-5">Dear [Customer's Name],</p>
+          <p className="mb-5">
+            Thank you for choosing Game Key Store for your gaming needs! We
+            appreciate your recent purchase and hope you're excited to dive into
+            your new game. Your satisfaction is our priority, and we're here to
+            assist you with any questions or concerns you may have. Don't
+            hesitate to reach out to our customer support team if you need
+            assistance. Enjoy your gaming experience, and we look forward to
+            serving you again in the future.
+          </p>
+          <p>Best regards, </p>
+          <p className="mb-5">Game Key Store Team</p>
+        </p>
+      </div>
+    </div>
+  );
 }
