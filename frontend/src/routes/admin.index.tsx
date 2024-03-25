52ff5874663a8e375879e8924758d0efe517c832
szabot1 <gleams-aquifer.0d@icloud.com>
2024-03-25 09:12:31 +0100

    chore: admin stat page redirects

--- frontend/src/routes/admin.index.tsx
+++ frontend/src/routes/admin.index.tsx
@@ -2,7 +2,7 @@ import { AdminRoute } from "@/components/auth/protected";
 import ErrorPage from "@/error-page";
 import { AdminStats, adminStatsQuery } from "@/lib/query/admin";
 import { useQuery } from "@tanstack/react-query";
-import { FileRoute, redirect } from "@tanstack/react-router";
+import { FileRoute, redirect, useNavigate } from "@tanstack/react-router";
 import { Helmet } from "react-helmet-async";
 
 export const Route = new FileRoute("/admin/").createRoute({
@@ -42,20 +42,48 @@ function Inner() {
         <title>Admin</title>
       </Helmet>
 
-      <Stat label="Tags" value={games.tags || 0} />
-      <Stat label="Games" value={games.games || 0} />
-      <Stat label="Sellers" value={games.sellers || 0} />
-      <Stat label="Offers" value={games.offers || 0} />
-      <Stat label="Users" value={games.users || 0} />
+      <Stat label="Tags" value={games.tags || 0} to="/admin/tags" />
+      <Stat label="Games" value={games.games || 0} to="/admin/games" />
+      <Stat label="Sellers" value={games.sellers || 0} to="/admin/users" />
+      <Stat label="Offers" value={games.offers || 0} to="/admin/users" />
+      <Stat label="Users" value={games.users || 0} to="/admin/users" />
     </section>
   );
 }
 
-function Stat({ label, value }: { label: string; value: number }) {
+const colors: Record<string, { accent: string }> = {
+  tags: { accent: "bg-blue-500" },
+  games: { accent: "bg-green-500" },
+  sellers: { accent: "bg-yellow-500" },
+  offers: { accent: "bg-indigo-500" },
+  users: { accent: "bg-red-500" },
+};
+
+function Stat({
+  label,
+  value,
+  to,
+}: {
+  label: string;
+  value: number;
+  to: string;
+}) {
+  const navigate = useNavigate();
+
+  const color = colors[label.toLowerCase()];
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 basis-full md:basis-[45%] lg:basis-[32%] p-4 bg-zinc-800 rounded-lg mx-4 lg:mx-0">
-      <h2 className="text-2xl font-bold">{value}</h2>
-      <p className="text-lg">{label}</p>
+    <div
+      className="flex flex-col items-center justify-center gap-2 basis-full md:basis-[45%] lg:basis-[32%] p-4 bg-zinc-800 rounded-lg mx-4 lg:mx-0 cursor-pointer"
+      role="button"
+      onClick={() => navigate({ to })}
+    >
+      <div
+        className={`w-16 h-16 rounded-full flex items-center justify-center ${color.accent}`}
+      >
+        <span className="text-white text-2xl font-bold">{value}</span>
+      </div>
+      <span className="text-center text-lg font-bold">{label}</span>
     </div>
   );
 }
